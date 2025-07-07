155b10612e5e40dc218b0f72e5a5774a9a74a7a3
CodeSannz <codesannz@gmail.com>
2025-07-08 03:04:50 +0700

    Create

---
+++ Dutamovie/build.gradle.kts
@@ -0,0 +1,23 @@
+version = 2
+
+cloudstream {
+    description = "DutaMovie"
+    language = "id"
+    authors = listOf("Hexated", "CodeSannz")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://github.com/CodeAxella/IndoStream/raw/refs/heads/master/Dutamovie/icon.png"
+}
