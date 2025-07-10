4fc6383828702a25b44e7a78bfd40c3f81146bdf
Sann <codesannz@gmail.com>
2025-07-11 05:04:59 +0700

    Update Pusatfilm

--- Pusatfilm/src/main/kotlin/com/pusatfilm/Pusatfilm.kt
+++ Pusatfilm/src/main/kotlin/com/pusatfilm/Pusatfilm.kt
@@ -1,10 +1,6 @@
 package com.pusatfilm
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.Episode
-import com.lagradost.cloudstream3.LoadResponse
-import com.lagradost.cloudstream3.TvSeriesLoadResponse
-import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.MainAPI
@@ -20,14 +16,14 @@ import org.jsoup.nodes.Element
 class Pusatfilm : MainAPI() {
 
     override var mainUrl = "https://pf21.site/"
-
+    private var directUrl: String? = null
     override var name = "Pusatfilm"
     override val hasMainPage = true
     override var lang = "id"
     override val supportedTypes =
-        setOf(TvType.Movie, TvType.TvSeries, TvType.Anime, TvType.AsianDrama)
+            setOf(TvType.Movie, TvType.TvSeries, TvType.Anime, TvType.AsianDrama)
 
-    override val mainPage =
+        override val mainPage =
             mainPageOf(
                     "film-terbaru/page/%d/" to "Film Terbaru",
                     "trending/page/%d/" to "Film Trending",
@@ -37,6 +33,7 @@ class Pusatfilm : MainAPI() {
                     "west-series/page/%d/" to "West Series",
                     "drama-china/page/%d/" to "Drama China",
             )
+
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val data = request.data.format(page)
         val document = app.get("$mainUrl/$data").document
@@ -49,15 +46,15 @@ class Pusatfilm : MainAPI() {
         val href = fixUrl(this.selectFirst("a")!!.attr("href"))
         val posterUrl = fixUrlNull(this.selectFirst("a > img")?.getImageAttr()).fixImageQuality()
         val quality =
-            this.select("div.gmr-qual, div.gmr-quality-item > a").text().trim().replace("-", "")
+                this.select("div.gmr-qual, div.gmr-quality-item > a").text().trim().replace("-", "")
         return if (quality.isEmpty()) {
             val episode =
-                Regex("Episode\\s?([0-9]+)")
-                    .find(title)
-                    ?.groupValues
-                    ?.getOrNull(1)
-                    ?.toIntOrNull()
-                    ?: this.select("div.gmr-numbeps > span").text().toIntOrNull()
+                    Regex("Episode\\s?([0-9]+)")
+                            .find(title)
+                            ?.groupValues
+                            ?.getOrNull(1)
+                            ?.toIntOrNull()
+                            ?: this.select("div.gmr-numbeps > span").text().toIntOrNull()
             newAnimeSearchResponse(title, href, TvType.TvSeries) {
                 this.posterUrl = posterUrl
                 addSub(episode)
@@ -72,8 +69,8 @@ class Pusatfilm : MainAPI() {
 
     override suspend fun search(query: String): List<SearchResponse> {
         val document =
-            app.get("${mainUrl}?s=$query&post_type[]=post&post_type[]=tv", timeout = 50L)
-                .document
+                app.get("${mainUrl}?s=$query&post_type[]=post&post_type[]=tv", timeout = 50L)
+                        .document
         val results = document.select("article.item").mapNotNull { it.toSearchResult() }
         return results
     }
@@ -86,38 +83,135 @@ class Pusatfilm : MainAPI() {
     }
 
     override suspend fun load(url: String): LoadResponse {
-        return super.load(url).apply {
-            when (this) {
-                is TvSeriesLoadResponse -> {
-                    val document = app.get(url).document
-                    this.episodes =
-                        document.select("div.vid-episodes a, div.gmr-listseries a")
+        val fetch = app.get(url)
+        directUrl = getBaseUrl(fetch.url)
+        val document = fetch.document
+
+        val title =
+                document.selectFirst("h1.entry-title")
+                        ?.text()
+                        ?.substringBefore("Season")
+                        ?.substringBefore("Episode")
+                        ?.trim()
+                        .toString()
+        val poster =
+                fixUrlNull(document.selectFirst("figure.pull-left > img")?.getImageAttr())
+                        ?.fixImageQuality()
+        val tags =
+                document.select("div.gmr-moviedata strong:contains(Genre:) > a").map { it.text() }
+
+        val year =
+                document.select("div.gmr-moviedata strong:contains(Year:) > a")
+                        .text()
+                        .trim()
+                        .toIntOrNull()
+        val tvType = if (url.contains("/tv/")) TvType.TvSeries else TvType.Movie
+        val description = document.selectFirst("div[itemprop=description] > p")?.text()?.trim()
+        val trailer = document.selectFirst("ul.gmr-player-nav li a.gmr-trailer-popup")?.attr("href")
+        val rating =
+                document.selectFirst("div.gmr-meta-rating > span[itemprop=ratingValue]")
+                        ?.text()
+                        ?.toRatingInt()
+        val actors =
+                document.select("div.gmr-moviedata").last()?.select("span[itemprop=actors]")?.map {
+                    it.select("a").text()
+                }
+
+        val recommendations =
+                document.select("div.idmuvi-rp ul li").mapNotNull { it.toRecommendResult() }
+
+        return if (tvType == TvType.TvSeries) {
+            val episodes =
+                    document.select("div.vid-episodes a, div.gmr-listseries a")
                             .map { eps ->
                                 val href = fixUrl(eps.attr("href"))
-                                val name = eps.attr("title")
+                                val name = eps.text()
                                 val episode =
-                                    "Episode\\s*(\\d+)"
-                                        .toRegex()
-                                        .find(name)
-                                        ?.groupValues
-                                        ?.get(1)
+                                        name.split(" ")
+                                                .lastOrNull()
+                                                ?.filter { it.isDigit() }
+                                                ?.toIntOrNull()
                                 val season =
-                                    "Season\\s*(\\d+)"
-                                        .toRegex()
-                                        .find(name)
-                                        ?.groupValues
-                                        ?.get(1)
+                                        name.split(" ")
+                                                .firstOrNull()
+                                                ?.filter { it.isDigit() }
+                                                ?.toIntOrNull()
                                 Episode(
-                                    href,
-                                    name,
-                                    season = season?.toIntOrNull(),
-                                    episode = episode?.toIntOrNull(),
+                                        href,
+                                        name,
+                                        season = if (name.contains(" ")) season else null,
+                                        episode = episode,
                                 )
                             }
                             .filter { it.episode != null }
-                }
+            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = description
+                this.tags = tags
+                this.rating = rating
+                addActors(actors)
+                this.recommendations = recommendations
+                addTrailer(trailer)
             }
-        }!!
+        } else {
+            newMovieLoadResponse(title, url, TvType.Movie, url) {
+                this.posterUrl = poster
+                this.year = year
+                this.plot = description
+                this.tags = tags
+                this.rating = rating
+                addActors(actors)
+                this.recommendations = recommendations
+                addTrailer(trailer)
+            }
+        }
+    }
+
+    override suspend fun loadLinks(
+            data: String,
+            isCasting: Boolean,
+            subtitleCallback: (SubtitleFile) -> Unit,
+            callback: (ExtractorLink) -> Unit
+    ): Boolean {
+
+        val document = app.get(data).document
+        val id = document.selectFirst("div#muvipro_player_content_id")?.attr("data-id")
+
+        if (id.isNullOrEmpty()) {
+            document.select("ul.muvipro-player-tabs li a").apmap { ele ->
+                val iframe =
+                        app.get(fixUrl(ele.attr("href")))
+                                .document
+                                .selectFirst("div.gmr-embed-responsive iframe")
+                                .getIframeAttr()
+                                ?.let { httpsify(it) }
+                                ?: return@apmap
+
+                loadExtractor(iframe, "$directUrl/", subtitleCallback, callback)
+            }
+        } else {
+            document.select("div.tab-content-ajax").apmap { ele ->
+                val server =
+                        app.post(
+                                        "$directUrl/wp-admin/admin-ajax.php",
+                                        data =
+                                                mapOf(
+                                                        "action" to "muvipro_player_content",
+                                                        "tab" to ele.attr("id"),
+                                                        "post_id" to "$id"
+                                                )
+                                )
+                                .document
+                                .select("iframe")
+                                .attr("src")
+                                .let { httpsify(it) }
+
+                loadExtractor(server, "$directUrl/", subtitleCallback, callback)
+            }
+        }
+
+        return true
     }
 
     private fun Element.getImageAttr(): String {
@@ -131,7 +225,7 @@ class Pusatfilm : MainAPI() {
 
     private fun Element?.getIframeAttr(): String? {
         return this?.attr("data-litespeed-src").takeIf { it?.isNotEmpty() == true }
-            ?: this?.attr("src")
+                ?: this?.attr("src")
     }
 
     private fun String?.fixImageQuality(): String? {
@@ -143,5 +237,4 @@ class Pusatfilm : MainAPI() {
     private fun getBaseUrl(url: String): String {
         return URI(url).let { "${it.scheme}://${it.host}" }
     }
-
 }
