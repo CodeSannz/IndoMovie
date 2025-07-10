eab19e74af5fe576784760528ccf30859350fe10
Sann <codesannz@gmail.com>
2025-07-11 00:59:09 +0700

    Update Ngefilm.kt

--- Ngefilm/src/main/kotlin/com/pusatfilm/Ngefilm.kt
+++ Ngefilm/src/main/kotlin/com/pusatfilm/Ngefilm.kt
@@ -1,31 +1,26 @@
 package com.ngefilm
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
-import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.mainPageOf
-import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.httpsify
-import com.lagradost.cloudstream3.utils.loadExtractor
-import java.net.URI
-import org.jsoup.nodes.Element
 
 class Ngefilm : MainAPI() {
 
     override var mainUrl = "https://new14.ngefilm.site/"
-    private var directUrl: String? = null
+
     override var name = "Ngefilm"
     override val hasMainPage = true
     override var lang = "id"
-    override var name = "Ngefilm"
-    override val mainPage = mainPageOf(
-        "/page/%d/?s&search=advanced&post_type=movie&index&orderby&genre&movieyear&country&quality=" to "Movies Terbaru",
-        "/page/%d/?s=&search=advanced&post_type=tv&index=&orderby=&genre=&movieyear=&country=&quality=" to "Series Terbaru",
-        "/page/%d/?s=&search=advanced&post_type=tv&index=&orderby=&genre=drakor&movieyear=&country=&quality=" to "Series Korea",
-        "/page/%d/?s=&search=advanced&post_type=tv&index=&orderby=&genre=&movieyear=&country=indonesia&quality=" to "Series Indonesia",
-    )
-
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries, TvType.AsianDrama)
+    override val mainPage =
+            mainPageOf(
+                    "/page/%d/?s&search=advanced&post_type=movie&index&orderby&genre&movieyear&country&quality=" to
+                            "Movies Terbaru",
+                    "/page/%d/?s=&search=advanced&post_type=tv&index=&orderby=&genre=&movieyear=&country=&quality=" to
+                            "Series Terbaru",
+                    "/page/%d/?s=&search=advanced&post_type=tv&index=&orderby=&genre=drakor&movieyear=&country=&quality=" to
+                            "Series Korea",
+                    "/page/%d/?s=&search=advanced&post_type=tv&index=&orderby=&genre=&movieyear=&country=indonesia&quality=" to
+                            "Series Indonesia",
+            )
 }
