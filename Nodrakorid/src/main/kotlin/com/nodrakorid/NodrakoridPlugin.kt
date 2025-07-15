8bfce834e17372173eb4ce95ba123ac1574ef39c
Sann <codesannz@gmail.com>
2025-07-15 16:18:32 +0700

    Update NodrakoridPlugin

--- Nodrakorid/src/main/kotlin/com/nodrakorid/NodrakoridPlugin.kt
+++ Nodrakorid/src/main/kotlin/com/nodrakorid/NodrakoridPlugin.kt
@@ -1,24 +1,17 @@
 package com.nodrakorid
 
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.Chillx
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class NodrakoridPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
+        // All providers should be added in this manner. Please don't edit the providers list
+        // directly.
         registerMainAPI(Nodrakorid())
-        registerExtractorAPI(FilelionsTo())
-        registerExtractorAPI(Likessb())
-        registerExtractorAPI(DbGdriveplayer())
-        registerExtractorAPI(Dutamovie21())
-        registerExtractorAPI(Embedwish())
-        registerExtractorAPI(Doods())
-        registerExtractorAPI(Lylxan())
-        registerExtractorAPI(Ghbrisk())
-        registerExtractorAPI(FilelionsOn())
-        registerExtractorAPI(Kotakajaib())
-        registerExtractorAPI(Uplayer())
+        registerExtractorAPI(Boosterx())
+        registerExtractorAPI(Chillx())
     }
 }
