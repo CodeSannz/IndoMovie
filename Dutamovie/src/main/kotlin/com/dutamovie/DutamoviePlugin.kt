84cffbe1d871f07c97ad4a3e85a10f2b47cccb1b
Sann <codesannz@gmail.com>
2025-07-15 12:50:18 +0700

    Update DutamoviePlugin

--- Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
@@ -1,24 +1,16 @@
 package com.dutamovie
 
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.JWPlayer
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class DutamoviePlugin: Plugin() {
+class DutaMoviePlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(DutaMovie())
-        registerExtractorAPI(FilelionsTo())
-        registerExtractorAPI(Likessb())
-        registerExtractorAPI(DbGdriveplayer())
-        registerExtractorAPI(Dutamovie21())
-        registerExtractorAPI(Embedwish())
-        registerExtractorAPI(Doods())
-        registerExtractorAPI(Lylxan())
-        registerExtractorAPI(Ghbrisk())
-        registerExtractorAPI(FilelionsOn())
-        registerExtractorAPI(Kotakajaib())
-        registerExtractorAPI(Uplayer())
+        registerExtractorAPI(Ryderjet())
+        registerExtractorAPI(JWPlayer())
+        registerExtractorAPI(Embedfirex())
     }
 }
